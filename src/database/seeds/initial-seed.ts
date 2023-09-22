0c5dfd0d79c7a2badd255c9ab952a8a5b92741da
alex-go-nguyen <alex.nguyen.goldenowl@gmail.com>
2023-09-22 15:55:30 +0700

    fix seeds

--- src/database/seeds/initial-seed.ts
+++ src/database/seeds/initial-seed.ts
@@ -5,8 +5,33 @@ import User from 'src/user/entities/user.entity';
 import { Venue } from 'src/venue/entities/venue.entity';
 import { Factory, Seeder } from 'typeorm-seeding';
 
-import categoryData from '../data/category.json';
-
+const categoryData = [
+  {
+    name: 'Sân 5',
+    description:
+      'Sân 5 là một loại sân bóng đá nhỏ, thích hợp cho trận đấu 5 người mỗi đội. Trò chơi tập trung vào kỹ thuật cá nhân và đòi hỏi sự nhanh nhẹn trong không gian hạn chế.',
+    thumbnail:
+      'https://shopconhantao.com.vn/assets/cms/uploads/images/chi-phi-lam-san-bong/chi-phi-lam-san-bong-co-nhan-tao3.jpg',
+  },
+  {
+    name: 'Sân 7',
+    description:
+      'Sân 7 là một tùy chọn trung bình giữa sân nhỏ 5 người và sân lớn 11 người. Với 7 người mỗi đội, trận đấu diễn ra trên một sân rộng hơn, đòi hỏi sự kết hợp và chiến thuật hơn.',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR_SzGusU_5fDMUvqaV1f6Vl0N_dgTTUNsgQ&usqp=CAU',
+  },
+  {
+    name: 'Sân 11',
+    description:
+      'Sân 11 là sân bóng đá truyền thống với 11 người mỗi đội. Đây là mô hình phổ biến nhất của bóng đá và đòi hỏi sự cơ động, sự phối hợp đội hình và khả năng chơi trong không gian rộng lớn.',
+    thumbnail: 'https://conhantaonguyengia.com/uploads/2015/03/Co-nhan-tao-san-bong-da-Nguyen-Gia-1.jpg',
+  },
+  {
+    name: 'Sân Futsal',
+    description:
+      'Sân Futsal là một sân nhỏ hơn dành riêng cho trò chơi futsal. Với 5 người mỗi đội, futsal tập trung vào kỹ thuật, điều khiển bóng, và trò chơi nhanh chóng trên một bề mặt sân nhân tạo.',
+    thumbnail: 'https://media.diadiem247.com/uploads/w900/2021/06/19/fu1.jpg',
+  },
+];
 export default class InitialDatabaseSeed implements Seeder {
   public async run(factory: Factory): Promise<void> {
     await factory(User)({ role: RoleEnum.Admin }).create({
